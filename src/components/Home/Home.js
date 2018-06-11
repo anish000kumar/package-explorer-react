e67f68a2cf6ef54db4f2eba52a4c8f1d5ad516e5
Anish Kumar <anish000kumar@gmail.com>
2018-06-12 00:09:03 +0530

    started home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,8 +3,20 @@ import React, { Component } from 'react'
 export default class Home extends Component {
   render() {
     return (
-      <div>
-          Home
+      <div className="explorer-home">
+          <div className="home--header">
+            <div className="left">
+            </div>
+            <div className="right">
+            </div>
+          </div>
+
+          <div className="home--body">
+            <div className="sidebar">
+            </div>
+            <div className="container">
+            </div>
+          </div>
       </div>
     )
   }
