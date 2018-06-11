43b247ac7d640bf99c58943c1fe5a267adfff64f
Gautam <gautam@Gautams-MacBook-Pro.local>
2018-06-12 01:40:08 +0530

    Initialize PackageDetail page

---
+++ src/components/PackageDetail/PackageDetail.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import SearchBar from './../SearchBar/SearchBar'
+import './PackageDetail.css'
+class PackageDetail extends Component {
+    render() {
+        return (
+            <div className="package-detail">
+                <div className="home--header">
+                    <div className="left">
+                        <div className="project-icon"></div>
+                        <div className="project-data">
+                            <h3>Hello World</h3>
+                            <p>https://github.com/anish000kumar/he.git</p>
+                        </div>
+                    </div>
+                    <div className="right">
+                        <SearchBar />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default PackageDetail;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './components/Home/Home';
 import registerServiceWorker from './registerServiceWorker';
+import PackageDetail from './components/PackageDetail/PackageDetail'
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+// ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(<PackageDetail />, document.getElementById('root'));
 registerServiceWorker();
