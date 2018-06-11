a13176677a6a36131a08749aa3f65889049c2cf1
Gautam <gautam@Gautams-MacBook-Pro.local>
2018-06-12 02:50:50 +0530

    Start sidebar for package-detail

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import Home  from './../components/Home/Home'
 import PackageDetail  from './../components/PackageDetail/PackageDetail'
+import Router from 'tiny-react-router'
 
 export default {
     '/'            : Home,
