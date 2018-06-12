e92c761131aa07833a6e34e9cac131e6c26ac5e1
Anish Kumar <anish000kumar@gmail.com>
2018-06-12 08:46:25 +0530

    completed dependency ui

--- src/components/Dependencies/Dependencies.js
+++ src/components/Dependencies/Dependencies.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
+import Dependency from './Dependency'
 import './Dependencies.css'
 
 export default class Dependencies extends Component {
   render() {
     return (
       <div className="dependencies">
-         <input type="text" className="search-input" placeholder="Search (cmd+f)" style={{width: '100%'}}/>
+         <input type="text" className="search-input" accessKey="d" placeholder="Search (cmd+f)" style={{width: '100%'}}/>
          <div className="dependencies-list">
-            
+            <Dependency />
+            <Dependency />
+            <Dependency />
+            <Dependency />
+            <Dependency />
+            <Dependency />
+            <Dependency />
+            <Dependency />
          </div>
       </div>
     )
--- src/components/Dependencies/Dependency.js
+++ src/components/Dependencies/Dependency.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 
 export default class Dependency extends Component {
+
+
   render() {
     return (
-      <div>
-        
+      <div className="dependency">
+        <span className="dependency--name"> {this.props.name|| 'react'} </span>
+        <div className="dependency--version">
+          <input type="text" className="version" defaultValue={this.props.version || '16.3.0'} ></input>
+          <span className="latest-version"> {this.props.latestVersion || '16.3.1'} </span>
+        </div>
+        <span className="dependency--close">X</span>
       </div>
     )
   }
