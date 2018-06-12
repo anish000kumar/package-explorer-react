5fd895cdf24d39d475dd04f43a67b2365345056f
Anish Kumar <anish000kumar@gmail.com>
2018-06-12 10:33:27 +0530

    added tabs mechanismc

--- src/components/Dependencies/Dependencies.js
+++ src/components/Dependencies/Dependencies.js
@@ -4,18 +4,15 @@ import './Dependencies.css'
 
 export default class Dependencies extends Component {
   render() {
+    debugger
     return (
       <div className="dependencies">
          <input type="text" className="search-input" accessKey="d" placeholder="Search (cmd+f)" style={{width: '100%'}}/>
          <div className="dependencies-list">
-            <Dependency />
-            <Dependency />
-            <Dependency />
-            <Dependency />
-            <Dependency />
-            <Dependency />
-            <Dependency />
-            <Dependency />
+            { this.props.dependencies?
+              this.props.dependencies.map(dep => <Dependency /> ):
+              'No depepndencies'
+            }
          </div>
       </div>
     )
