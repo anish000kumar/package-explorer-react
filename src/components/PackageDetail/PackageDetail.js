6a55255e201638740e8a1161d62a9c2f60ee0f7c
Anish Kumar <anish000kumar@gmail.com>
2018-06-13 22:00:40 +0530

    added caret

--- src/components/PackageDetail/PackageDetail.js
+++ src/components/PackageDetail/PackageDetail.js
@@ -5,7 +5,7 @@ import './PackageDetail.css'
 
 const LeftAngle = () => (
 <div>
-    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" fill="#fff" width="30" height="65"  preserveAspectRatio="xMidYMid meet" viewBox="0 0 608 1280"><path d="M595 288q0 13-10 23L192 704l393 393q10 10 10 23t-10 23l-50 50q-10 10-23 10t-23-10L23 727q-10-10-10-23t10-23l466-466q10-10 23-10t23 10l50 50q10 10 10 23z" fill="#626262"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" fill="#fff" width="30" height="65"  preserveAspectRatio="xMidYMid meet" viewBox="0 0 608 1280"><path d="M595 288q0 13-10 23L192 704l393 393q10 10 10 23t-10 23l-50 50q-10 10-23 10t-23-10L23 727q-10-10-10-23t10-23l466-466q10-10 23-10t23 10l50 50q10 10 10 23z" fill="#fff"/></svg>
 </div>
 )
 class PackageDetail extends Component {
